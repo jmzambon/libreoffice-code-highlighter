a5c425a02df646bf7e2c913db3f2271df4b976ed
jmzambon <jeanmarczambon@gmail.com>
2024-08-11 08:58:55 +0200

    Reorganize locales structure.

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-cd codehighlighter
-
 # generate-mo-files.sh
 for PO_FILE in locales/*/LC_MESSAGES/*.po
 do
     MO_FILE="${PO_FILE/.po/.mo}"
-    msgfmt -o "$MO_FILE" "$PO_FILE"
+    msgfmt -o "codehighlighter/$MO_FILE" "$PO_FILE"
 done
 
 # generate codehighlighter2.oxt
+cd codehighlighter
+
 zip -r codehighlighter2.oxt .
 cd ..
 mv codehighlighter/codehighlighter2.oxt ./
